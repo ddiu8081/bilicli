ca8b8d87caaaab0ac7bf8a057f18821394cb5332
ddiu8081 <ddiu8081@gmail.com>
2022-10-25 01:16:42 +0800

    feat(live-mini): 增加抽奖/弹幕类型弹幕标识

--- packages/live-mini/src/component/msg/danmu.ts
+++ packages/live-mini/src/component/msg/danmu.ts
@@ -1,6 +1,13 @@
+import ansis from 'ansis'
 import type { DanmuMsg } from 'blive-message-listener'
 import { usernameCom } from '../username'
 
 export const danmuMsgCom = (msg: DanmuMsg) => {
-  return `${usernameCom(msg.user)}: ${msg.content}`
+  let msgType = ''
+  if (msg.emoticon) {
+    msgType = '[表情]'
+  } else if (msg.lottery) {
+    msgType = '[抽奖]'
+  }
+  return `${usernameCom(msg.user)}: ${ansis.yellow(msgType)} ${msg.content}`
 }
